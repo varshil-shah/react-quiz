6a2ff2403a77291c49a50a4010d3d56e8b9261d7
Varshil Shah <varshilshah.in@gmail.com>
2024-01-02 19:51:44 +0530

    Implement a handler to handle next question

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Loader from "./components/Loader";
 import Error from "./components/Error";
 import StartScreen from "./components/StartScreen";
 import Question from "./components/Question";
+import NextButton from "./components/NextButton";
 
 const initialState = {
   questions: [],
@@ -30,6 +31,8 @@ function reducer(state, action) {
         answer: action.payload,
         points: isCorrect ? state.points + question.points : state.points,
       };
+    case "nextQuestion":
+      return { ...state, index: state.index + 1, answer: null };
     default:
       throw new Error(`Unknown action: ${action.type}`);
   }
@@ -61,11 +64,14 @@ function App() {
           <StartScreen numOfQuestions={numOfQuestions} dispatch={dispatch} />
         )}
         {status === "active" && (
-          <Question
-            question={questions[index]}
-            dispatch={dispatch}
-            answer={answer}
-          />
+          <>
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <NextButton dispatch={dispatch} answer={answer} />
+          </>
         )}
       </Main>
     </div>

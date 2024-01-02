7a172237a04c2bea77a135b2b391f8e92f173e65
Varshil Shah <varshilshah.in@gmail.com>
2024-01-02 17:57:38 +0530

    Loading data from fake API using json-server

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
+const initialState = {
+  questions: [],
+  status: "loading", // loading, error, ready, active, finished
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error(`Unknown action: ${action.type}`);
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/questions").then((response) =>
+      response
+        .json()
+        .then((data) => dispatch({ type: "dataReceived", payload: data }))
+        .catch((error) => dispatch({ type: "dataFailed" }))
+    );
+  }, []);
+
   return (
     <div className="app">
       <Header />
